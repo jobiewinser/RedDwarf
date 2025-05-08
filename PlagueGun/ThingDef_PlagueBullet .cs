e47633c76d1d25c14cb9b15cffbbfa4fb8aa8c27
Jobie Winser <jobiewinser@live.co.uk>
2025-05-08 15:31:08 +0100

    init

---
+++ PlagueGun/ThingDef_PlagueBullet .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Jobie.Plague
+{
+    public class ThingDef_PlagueBullet : ThingDef
+    {
+        public float AddHediffChance = 0.05f; //The default chance of adding a hediff.
+        public HediffDef HediffToAdd = HediffDefOf.Plague;
+    }
+}
