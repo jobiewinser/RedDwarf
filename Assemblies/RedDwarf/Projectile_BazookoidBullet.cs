c58302ed467077259eec8f6adac7068db333161c
Jobie Winser <jobiewinser@live.co.uk>
2025-05-08 17:09:27 +0100

    added bazookoid

---
+++ Assemblies/RedDwarf/Projectile_BazookoidBullet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Jobie.RedDwarf
+{
+    public class Projectile_BazookoidBullet : Bullet
+    {
+        #region Properties
+        //
+        public ThingDef_BazookoidBullet Def
+        {
+            get
+            {
+                return this.def as ThingDef_BazookoidBullet;
+            }
+        }
+        #endregion Properties
+    }
+}
---
+++ Assemblies/RedDwarf/ThingDef_BazookoidBullet .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Jobie.RedDwarf
+{
+    public class ThingDef_BazookoidBullet : ThingDef
+    {
+        // nothing special here
+    }
+}
